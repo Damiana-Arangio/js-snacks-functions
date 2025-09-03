69bf4fc119f9ed1ae7a68682b14794c9892593a8
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-03 22:33:51 +0200

    Implementazione esercizio "04_conta_vocali" completata

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function contaVocali(stringa) {
+    let contatore = 0;
+    
+    // Per ogni lettera controllo se è una vocale
+    for (i = 0; i < stringa.length ; i++) {
+        if (stringa[i] === "a" || stringa[i] === "e" || stringa[i] === "i" || stringa[i] === "o" || stringa[i] === "u") {
+            contatore +=1;
+        }
+    }
 
+    return contatore;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const num = contaVocali(word);
+console.log("La parola " + word + " contiene " + num + " vocali ");
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
