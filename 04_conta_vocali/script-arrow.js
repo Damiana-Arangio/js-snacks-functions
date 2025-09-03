f396b149d3d9db5a3ca36a982adf271e38e52126
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-04 00:01:50 +0200

    Implementazione "04-conta_vocali" con arrow function (bonus)

---
+++ 04_conta_vocali/script-arrow.js
@@ -0,0 +1,25 @@
+/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+
+const word = 'javascript';
+
+
+// Dichiara la funzione qui.
+const contaVocali = (stringa) => {
+    let contatore = 0;
+
+    // Per ogni lettera controllo se è una vocale
+    for (i = 0; i < stringa.length; i++) {
+        if (stringa[i] === "a" || stringa[i] === "e" || stringa[i] === "i" || stringa[i] === "o" || stringa[i] === "u") {
+            contatore += 1;
+        }
+    }
+
+    return contatore;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+const num = contaVocali(word);
+console.log("La parola " + word + " contiene " + num + " vocali ");
+
+
+//Risultato atteso se si passa 'javascript': 3 (a, a, i)
