cf82890d40a54f0aefe7091a4902a2456a23e805
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-03 23:08:53 +0200

    implementazione esercizio "05_bonus" completata

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,25 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function saluta(name) {
+    const dataOggi = new Date();            // Restituisce un oggetto data con la data e l'ora correnti.
+    const orario = dataOggi.getHours();     // Restituisce l'orario corrente, come numero (0-23), dell'oggetto "dataOggi"
+    
+    if (orario >= 6 && orario <= 13) {      // Orario compreso nel range 6-13
+        return "Buongiorno " + name;
+    }
 
+    else if (orario > 13 && orario <= 17) {
+        return "Buon pomeriggio " + name;
+    }
+
+    else {
+        return "Buonasera " + name;
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluta(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
