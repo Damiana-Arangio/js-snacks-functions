fad2deaac766f0e0c071c0e7c1d240f809d21696
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-03 23:55:11 +0200

    Correzione script "01_saluto" commentato + implementazione "02-iniziali" con arrow function

---
+++ 02_iniziali/script-arrow.js
@@ -0,0 +1,29 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+const estraiInizialiNomi = (array) => {
+
+    let iniziali = [];
+    let elemento;
+    let lettera;
+
+    for (let i = 0; i < array.length; i++) {
+
+        elemento = array[i];        // Copio l'elemento corrente dell'array nella variabile elemento
+        lettera = elemento[0];      // Estraggo la prima lettera dalla stringa contenuta in elemento
+        iniziali.push(lettera);     // Aggiungo la lettera ricavata al nuovo array "iniziali"
+    }
+
+    return iniziali;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(estraiInizialiNomi(names));
+
+// console.log(estraiInizialiNomi(names));
+
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
